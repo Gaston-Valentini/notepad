38bd531762964f3cd3610f98f5f44fa37b0aa07a
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-01-09 12:03:44 +0100

    Add: User model created.

---
+++ server/src/models/User.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        default: () => `User-${Date.now().toString()}`,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model("User", userSchema);
+
+export { User };
