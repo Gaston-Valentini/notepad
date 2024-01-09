c342af13c4f59ff78c1c471026b8338f4ccb6ae7
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-01-09 12:16:34 +0100

    Add: Environment variables configured and database connection string and server port modified.

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,8 +1,10 @@
 import { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const database = async () => {
     try {
-        await connect("mongodb+srv://root:oFM3QfYNYegCH5kN@cluster0.dlxlkgm.mongodb.net/notepad");
+        await connect(process.env.DATABASE);
         console.log(`Connected to database "notepad"`);
     } catch (error) {
         console.error(`An error occurred in the connection to the database: ${error}`);
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 import { database } from "./database/database.js";
 import { app } from "./app/app.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const startApp = async () => {
     try {
         await database();
         app.listen(3000, () => {
-            console.log(`Server listening on port 3000`);
+            console.log(`Server listening on port ${process.env.PORT}`);
         });
     } catch (error) {
         console.error(`An error occurred during server initialization: ${error}`);
